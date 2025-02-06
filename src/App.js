f5c39f2e9da71a6efb811905de9a5b9246c4ad87
Gustavo Rojas <115680051+piano7check@users.noreply.github.com>
2025-02-06 01:23:41 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Presentation from './components/Presentation';
+
+function App() {
+  return (
+    <div className="App">
+      <Presentation></Presentation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Presentation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Presentation = () => {
+  return (
+    <div className="cv-container">
+      <img src="image.jpg" alt="Foto de Gustavo Rojas Valdivia"/>
+      <h1>Gustavo Rojas Valdivia</h1>
+      <p>Email: <a href="mailto:gustavo@email.com">gustavo@email.com</a></p>
+    </div>
+  )
+}
+
+export default Presentation
