8a9731bced733ad760fc9235b6a9855143d3049e
Gustavo Rojas <115680051+piano7check@users.noreply.github.com>
2025-02-08 18:42:46 +0000

    Refactored styles and fixed image path in Presentation

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './styles.css'; // Importamos el archivo de estilos
 
 const Presentation = () => {
   return (
     <div className="cv-container">
-      <img src="image.jpg" alt="Foto de Gustavo Rojas Valdivia"/>
+      <img className="cv-image" src={process.env.PUBLIC_URL + "/image.jpg"} alt="Foto de Gustavo Rojas Valdivia"/>
       <h1>Gustavo Rojas Valdivia</h1>
       <p>Email: <a href="mailto:gustavo@email.com">gustavo@email.com</a></p>
     </div>
-  )
+  );
 }
 
-export default Presentation
+export default Presentation;
